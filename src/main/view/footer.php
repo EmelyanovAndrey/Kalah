d584411b9e363dc275cf71846ad34ced98682351
Илья <ilykonst95@gmail.com>
2016-10-21 21:20:10 +0400

    Optimized
    Procesed imports to .min

--- src/main/view/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="row">
-        <div class="col s3 offset-s9"> 
-            <a href="mailto:ilykonst95@gmail.com">Web-master: admin@ilyakonst.com</a>
-            <p>&copy;2016 Константинов Илья</p>
-        </div>
-    </div>
-</footer>
