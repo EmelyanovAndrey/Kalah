ff2aa78474abaccf82f77812716a4822d7d1f007
Илья <ilykonst95@gmail.com>
2016-12-13 19:37:39 +0400

    game page js

--- webapp/src/js/game_page.js
+++ webapp/src/js/game_page.js
@@ -16,4 +16,21 @@ $(document).ready(function() {
   var lunka = $('.board .lunka');
 
   annimation(lunka);
+
+
+  var board = $('#board');
+  var enemy_list = board.find('#enemy_list');
+  var gamer_list = board.find('#gamer_list');
+
+  enemy_list.append('<td rowspan=2 class="kalah enemy-obj" id="enemy"><span>0</span></td>');
+
+  for (var i = 0; i  < 6; i++) {
+    enemy_list.append('<td class="lunka enemy-obj" id="enemy_' + i + '"><span>6</span></td>');
+  }
+
+  enemy_list.append('<td rowspan=2 class="kalah" id="gamer"><span>0</span></td>');
+
+  for (var i = 0; i  < 6; i++) {
+    gamer_list.append('<td class="lunka" id="gamer_' + i + '"><span>6</span></td>');
+  }
 });
