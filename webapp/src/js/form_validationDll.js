518e6c1e232a3b3a1c7f8d53abef7dbbab714bb8
Илья <ilykonst95@gmail.com>
2016-12-14 10:29:34 +0400

    refractoring

--- webapp/src/js/form_validationDll.js
+++ webapp/src/js/form_validationDll.js
@@ -19,78 +19,79 @@ var re_passValid = function(pass, re_pass) {
 };
 
 var loginValid = function () {
-    $('#loginin').find('#login').click(function() {
-        var name = $('#loginin').find('#name');
-        var password = $('#loginin').find('#password');
+    var name = $('#loginin').find('#name');
+    var password = $('#loginin').find('#password');
 
-        characterValid(name, name.attr('minlength'), name.attr('maxlength'));
-        characterValid(password, password.attr('minlength'), password.attr('maxlength'));
+    characterValid(name, name.attr('minlength'), name.attr('maxlength'));
+    characterValid(password, password.attr('minlength'), password.attr('maxlength'));
 
-        if (name.hasClass('valid') && password.hasClass('valid')) {
-            return true;
-        }
-        else return false;
-    });
+    if (name.hasClass('valid') && password.hasClass('valid')) {
+        return true;
+    }
+    else return false;
 };
 
 var registrValid = function () {
-    $('#registration').find('#signup').click(function() {
-        var re_pass = $('#registration').find('#re-password');
-        var password = $('#registration').find('#password');
-        var name = $('#registration').find('#name');
-        var email = $('#registration').find('#email');
-        var ava = $('#registration').find('#icons');
+    var re_pass = $('#registration').find('#re-password');
+    var password = $('#registration').find('#password');
+    var name = $('#registration').find('#name');
+    var email = $('#registration').find('#email');
+    var ava = $('#registration').find('#icons');
 
-        characterValid(name, name.attr('minlength'), name.attr('maxlength'));
-        characterValid(password, password.attr('minlength'), password.attr('maxlength'));
-        characterValid(email, email.attr('minlength'), email.attr('maxlength'));
-        re_passValid(password, re_pass);
+    characterValid(name, name.attr('minlength'), name.attr('maxlength'));
+    characterValid(password, password.attr('minlength'), password.attr('maxlength'));
+    characterValid(email, email.attr('minlength'), email.attr('maxlength'));
+    re_passValid(password, re_pass);
 
-        if (name.hasClass('valid') && email.hasClass('valid') && password.hasClass('valid') && re_pass.hasClass('valid') && ava.val() !== null) {
-            return true;
-        } else {
-            return false;
-        }
-    });
+    if (name.hasClass('valid') && email.hasClass('valid') && password.hasClass('valid') && re_pass.hasClass('valid') && ava.val() !== null) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 var changeProfileValid = function () {
-    $('#profile').find('#change').click(function() {
+    var name = $('#profile').find('#name');
+    var password = $('#profile').find('#password');
+    var re_pass = $('#profile').find('#re-password');
+    var email = $('#profile').find('#email');
 
-        var name = $('#profile').find('#name');
-        var password = $('#profile').find('#password');
-        var re_pass = $('#profile').find('#re-password');
-        var email = $('#profile').find('#email');
+    characterValid(name, name.attr('minlength'), name.attr('maxlength'));
+    characterValid(password, password.attr('minlength'), password.attr('maxlength'));
+    characterValid(email, email.attr('minlength'), email.attr('maxlength'));
+    re_passValid(password, re_pass);
 
-        characterValid(name, name.attr('minlength'), name.attr('maxlength'));
-        characterValid(password, password.attr('minlength'), password.attr('maxlength'));
-        characterValid(email, email.attr('minlength'), email.attr('maxlength'));
-        re_passValid(password, re_pass);
-
-        if (name.hasClass('valid') && email.hasClass('valid') && password.hasClass('valid') && re_pass.hasClass('valid')) {
-            return true;
-        } else {
-            return false;
-        }
-    });
+    if (name.hasClass('valid') && email.hasClass('valid') && password.hasClass('valid') && re_pass.hasClass('valid')) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 $(document).ready(function() {
-    if (loginValid()) {
-        console.log('login: true');
-    }
-    else console.log('login: false')
-
-
-    if (registrValid()) {
-        console.log('regist: true');
-    } else {
-        console.log('regist: false');
-    }
-
-    if (changeProfileValid()) {
-        console.log('change: true');
-    } else {
-        console.log('change: false');
-    }
+    // $('#loginin').find('#login').click(function() {
+    //
+    //     if (loginValid()) {
+    //      console.log('login: true');
+    //     }
+    //     else console.log('login: false')
+    // });
+    //
+    // $('#registration').find('#signup').click(function() {
+    //
+    //     if (registrValid()) {
+    //         console.log('regist: true');
+    //     } else {
+    //         console.log('regist: false');
+    //     }
+    // });
+    //
+    // $('#profile').find('#change').click(function() {
+    //
+    //     if (changeProfileValid()) {
+    //         console.log('change: true');
+    //     } else {
+    //         console.log('change: false');
+    //     }
+    // });
 });
---
+++ webapp/src/js/profileImg.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ilya on 14.12.16.
+ */
+var profile_img_rendering = function (src) {
+    $('#profile_img').append('<img src="'+ src + '">')
+}
+
